cbff534ccd472649b37e4ee9ff0e7395c44d6876
Alejandro Marquino <90139066+AlejandroMarquino@users.noreply.github.com>
2022-10-23 12:33:09 +0200

    Add files via upload

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -6,8 +6,36 @@
 /*   By: almarqui <almarqui@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 17:21:38 by almarqui          #+#    #+#             */
-/*   Updated: 2022/10/22 17:21:44 by almarqui         ###   ########.fr       */
+/*   Updated: 2022/10/23 09:18:07 by almarqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 
+void	ft_print_comb(void)
+{
+	int	a;
+	int	b;
+	int	c;
+
+	a = '0';
+	while (a <= '7')
+	{
+		b = a + 1;
+		while (b <= '8')
+		{
+		c = b + 1;
+			while (c <= '9')
+			{
+				write (1, &a, 1);
+				write (1, &b, 1);
+				write (1, &c, 1);
+				if (a != '7')
+					write (1, ", ", 2);
+			c++;
+			}
+		b++;
+		}
+	a++;
+	}
+}
