c40a49992391e5f1d042be8df6953ff2b8feee37
Alejandro Marquino <90139066+AlejandroMarquino@users.noreply.github.com>
2022-10-24 17:52:43 +0200

    Add files via upload

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -6,8 +6,40 @@
 /*   By: almarqui <almarqui@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 12:34:15 by almarqui          #+#    #+#             */
-/*   Updated: 2022/10/23 12:34:22 by almarqui         ###   ########.fr       */
+/*   Updated: 2022/10/24 17:50:20 by almarqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
+void	ft_print_combo2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_putchar(a / 10 + 48);
+			ft_putchar(a % 10 + 48);
+			ft_putchar(' ');
+			ft_putchar(b / 10 + 48);
+			ft_putchar(b % 10 + 48);
+			ft_putchar(',');
+			b++;
+		}
+		a++;
+	}
+}
+// int	main()
+// {
+// 	ft_print_combo2();
+// 	return(0);
+// }
