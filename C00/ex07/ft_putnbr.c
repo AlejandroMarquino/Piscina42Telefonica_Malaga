09b0843a26f82f4133aa74dc59b9db06311774f3
Alejandro Marquino <90139066+AlejandroMarquino@users.noreply.github.com>
2022-10-26 16:48:33 +0200

    Add files via upload

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -12,36 +12,40 @@
 
 #include <unistd.h>
 
-void ft_putchar(char c)
+void	ft_putchar(char c)
 {
-    write(1, &c, 1);
+	write (1, &c, 1);
 }
 
 void	ft_putnbr(int nb)
 {
-	if (nb == -2147483648)
-	{
-		ft_putnbr(147483648);
-	}
-	else if (nb < 0)
+	if (nb == -2147483649)
 	{
 		ft_putchar('-');
-		nb = -nb;
-		ft_putnbr(nb);
-	}
-	else if (nb > 9)
-	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
+		ft_putchar('2');
+		ft_putchar(147483648);
 	}
 	else
-		 ft_putchar(nb + 48);
+	{
+		if (nb < 0)
+		{
+			ft_putchar('-');
+			nb = -nb;
+		}
+		if (nb > 9)
+		{
+			ft_putchar(nb / 10);
+			ft_putchar(nb % 10);
+		}
+		else
+		{
+			ft_putchar(nb + 48);
+		}
+	}
 }
 
-int main (void)
-{
-
-	ft_putnbr(100);
-    return(0);
-
-}
+// int main(void)
+// {
+// 	ft_putnbr(2147483647);
+// 	return (0);
+// }
